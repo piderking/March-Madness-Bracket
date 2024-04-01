86fe771e620bc4c06499748371ce6465fb07c1e4
Gravity-Developer <piderking8@gmail.com>
2024-04-01 16:06:10 -0700

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex-1 bg-lime-400"></div>
+      <div className="flex-1 bg-slate-300"></div>
+
+    </div>
+  )
+}
